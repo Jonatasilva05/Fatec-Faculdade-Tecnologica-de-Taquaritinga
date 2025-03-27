128c902eb2353c68da55cd252745eb1b4c7dafd6
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-03-27 10:45:32 -0300

    27-03-25

--- robson/aula6/settings.py
+++ robson/aula6/settings.py
@@ -5,3 +5,10 @@ class Settings:
         self.bg_color = (230,230,230)
 
         self.velocidade_nave = 1.5
+
+        #Configuração das balas
+        self.velocidade_missel = 2.0
+        self.largura_missel = 3
+        self.altura_missel = 15
+        self.cor_missel = (60, 60, 60)
+        self.disparos_por_vez = 3
