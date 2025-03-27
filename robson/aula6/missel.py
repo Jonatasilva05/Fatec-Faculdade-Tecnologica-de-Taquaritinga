01708d472108ae037c63fadd22c7fbaf4d8c179e
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-03-27 11:35:09 -0300

    27-03-25

--- robson/aula6/missel.py
+++ robson/aula6/missel.py
@@ -11,3 +11,8 @@ class Missel(Sprite):
         self.screen = ia_game.screen # Obtém a tela de jogo para onde o missil sera desenhado
         self.settings = ia_game.settings # acessa as configurações do jogo, incluindo velocidade e cor do missil
         self.color = self.settings.cor_missel # define a cor do missil a partir das configurações
+        self.rect = pygame.Rect(0, 0, self.settings.largura_missel, self.settings.altura_missel)
+
+        self.rect.midtop = ia_game.nave.rect.midtop
+
+        self.y = float(self.rect.y)
