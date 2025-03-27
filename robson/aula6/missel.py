2aad97bdb6c737817838d4b899e20283077f6c9b
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-03-27 10:53:41 -0300

    27-03-25

---
+++ robson/aula6/missel.py
@@ -0,0 +1,13 @@
+import pygame 
+from pygame.sprite import Sprite  #importa a classe Sprite, que serve como base para o objeto missel
+
+class Missel(Sprite):
+    # classe que representa um míssil disparado pela nave, herda de Sprite
+
+    def __init__(self, ia_game):
+        # inicializa o míssil e define sua posição inicial
+        
+        super().__init__() # chama o construtor da classe mãe (Sprite)
+        self.screen = ia_game.screen # Obtém a tela de jogo para onde o missil sera desenhado
+        self.settings = ia_game.settings # acessa as configurações do jogo, incluindo velocidade e cor do missil
+        self.color = self.settings.cor_missel # define a cor do missil a partir das configurações
