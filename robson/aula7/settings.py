57823dc46e4d25056ff611c060408dac0be3bb2b
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-04-03 10:19:42 -0300

    03-04-25

---
+++ robson/aula7/settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    def __init__(self):
+        self.largura_tela = 1200    
+        self.altura_tela = 740    
+        self.bg_color = (230,230,230)
+
+        self.velocidade_nave = 1.5
+
+        #Configuração das balas
+        self.velocidade_missel = 2.0
+        self.largura_missel = 3
+        self.altura_missel = 15
+        self.cor_missel = (60, 60, 60)
+        self.disparos_por_vez = 3
