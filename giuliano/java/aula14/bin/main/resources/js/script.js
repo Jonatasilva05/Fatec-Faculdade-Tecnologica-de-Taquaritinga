ae49b5ae59a8d77f9e2c684c3b11f6a1e45145b4
Jonatasilva05 <jonatasmoraes05@gmail.com>
2025-06-10 11:54:37 -0300

    10-06-25

--- giuliano/java/aula14/bin/main/resources/js/script.js
+++ giuliano/java/aula14/bin/main/resources/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(()=> {
     $('#tabela').on('click', '.excluir', (evento)=> {
         let codigo = evento.target.getAttribute('rel');
-        alert(codigo);  
+        if(confirm('Tem certeza que deseja excluir o livro?')) {
+            let url = 'http://localhost:9000/excluirlivro?codigo='+ codigo;
+            
+        }
     });
 });
